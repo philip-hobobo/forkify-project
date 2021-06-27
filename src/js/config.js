8dd4d43111f8d4b5b45d4f2a0354be315b6e4be1
philip-hobobo <philip_hobobo@outlook.com>
2021-06-27 14:17:59 -0700

    First commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '49bb010b-88f7-46d4-b34f-b181deb4e1c3';
+export const MODAL_CLOSE_SEC = 2.5;
